6633b1bb61f9c0272d2f12e6da9433318ea37e2b
Razvan Zarnescu <zarnescurazvan98@gmail.com>
2023-03-02 10:21:15 +0200

    Push Project

---
+++ src/products.js
@@ -0,0 +1,66 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.png";
+import product7 from "./assets/products/7.png";
+import product8 from "./assets/products/8.png";
+import product9 from "./assets/products/9.png";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Laptop HP",
+    price: 999.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "NOC TV",
+    price: 999.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Gaming PC",
+    price: 1899.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Panasonic TV",
+    price: 1399.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Gaming PC",
+    price: 1799.0,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "MacBook Pro",
+    price: 2199.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Laptop Lenovo",
+    price: 1299.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Laptop HP Gaming",
+    price: 1699.0,
+    productImage: product8,
+  },
+  {
+    id: 9,
+    productName: "Gaming PC",
+    price: 1499.0,
+    productImage: product9,
+  }
+];
